156890f29fe049211dc1f03de45927d1456248ec
JishnuSharma <jishnusharma18@gmail.com>
2024-01-04 01:15:29 +0530

    Basic build for Landing page of the website

---
+++ index work.js
@@ -0,0 +1,22 @@
+let images = ["images/main page products/display laptop.jpg","images/main page products/display headphones.jpeg","images/main page products/display cosmetics.jpg"];
+let links = ["https://www.microsoft.com/en-in/surface/devices/surface-pro-9?activetab=pivot:overviewtab","https://www.boat-lifestyle.com/products/rockerz-551-anc-noise-cancelling-headphone","https://www.nykaa.com/maybelline-new-york-superstay-vinyl-ink-liquid-lipstick/p/7066987?productId=7066987&pps=1&skuId=7066975"];
+let imageIndex = 0;
+
+function changeDisplay()
+{
+   let target = document.getElementById('topproduct');
+   let link = document.getElementById('displink');
+   target.style.opacity = 0;
+
+   setTimeout(function()
+   {
+    target.src = images[imageIndex];
+    link.href = links[imageIndex];
+    target.style.opacity = 1;
+   }, 400);
+
+   imageIndex = (imageIndex + 1)%images.length;
+}
+
+setInterval(changeDisplay,5000);
+
